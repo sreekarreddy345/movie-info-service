bb5d7bc8d0abacfa946e4fc591659419f899b249
Sreekar Reddy <sreekarreddy345@gmail.com>
2020-04-29 18:02:18 -0500

    first commit

---
+++ src/main/java/com/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,28 @@
+package com.movieinfoservice.resources;
+
+import com.movieinfoservice.models.Movie;
+import com.movieinfoservice.models.MovieSummary;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+    @Value("${api.key}")
+    private String apiKey;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("movieId") String movieId) {
+        MovieSummary movieSummary =
+                restTemplate.getForObject("https://api.themoviedb.org/3/movie/" + movieId + "?api_key=" + apiKey, MovieSummary.class);
+        return new Movie(movieId, movieSummary.getTitle(), movieSummary.getOverview());
+    }
+//    https://api.themoviedb.org/3/movie/550?api_key=5d37e8aa6083fdb24d5d3a7fafaa8a2a
+}
